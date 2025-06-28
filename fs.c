f5027cefeb37a936229ffd37f98f58d4aeb6bf78
Tomas Menchaca <atm7812@mavs.uta.edu>
2025-06-28 03:12:38 -0500

    worling on trying to implement formatfs

--- fs.c
+++ fs.c
@@ -4,7 +4,23 @@
 #include <string.h>
 #define BLOCK_SIZE 512
 
+typedef struct{
+    int fileSize;
+    time_t  mod;
+    int blockptr;
+    char username[50];
+}DABPT;
 
+typedef struct{
+    char filename[54];
+    int inodeptr;
+}FNT;
+
+typedef struct{
+    int blockptrs[8];
+}BPTS;
+
+// creatfs 
 void create_fs(char* name, int numBlocks){
 
     FILE* fp;
@@ -28,4 +44,35 @@ void create_fs(char* name, int numBlocks){
 
 
 }
+
+// formatfs
+void format_fs(char* name, int numOfFilenames, int numOfDabpt){
+
+    FILE* fp;
+    fp = fopen(name, "r+b");
+
+    if(fp != NULL){
+        printf("Disk file opened successfully!\n");
+    }
+    else{
+        printf("Unable to open disk file!\n");
+    }
+
+    // Calculating how many FNT entries fit in one block
+    int fntentries = BLOCK_SIZE / sizeof(FNT);
+
+    int numFNT = (numOfFilenames + fntentries - 1) / fntentries;
+
+    // Calculating how many DABPT entries per block
+    int dabptentries = BLOCK_SIZE / sizeof(DABPT);
+
+    int numDABPT = (numOfDabpt + dabptentries - 1) / dabptentries;
+
+    int fntstart = 0;
+    int dabptstart = fntstart + numFNT;
+    int bptrstart = dabptstart + numDABPT;
+
+
+}
+
 
